ac8fe018958b3600c79d396206790d79d726eac7
takidilmi <angeldilmi@gmail.com>
2023-08-29 22:51:25 +0200

    Compelted assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+function submitData(name, email) {
+    const data = {
+        name: name,
+        email: email
+    };
+    
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    }    
+    
+    return fetch("http://localhost:3000/users", requestOptions)
+    .then(function(response) {
+        return response.json();  
+    })
+    .then(function(data){
+        const newId = data.id;
+        const idElement = document.createElement('p');
+        idElement.textContent = 'New ID: ' +newId;
+        document.body.appendChild(idElement);
+        return data;
+    })
+    .catch(function(error) {
+        const errorElement = document.createElement('p');
+        errorElement.textContent = 'Error: ' + error.message;
+        document.body.appendChild(errorElement);
+    });
+
+}
